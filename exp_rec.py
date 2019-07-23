e31a7644e5119c2d734f3e80488b1021d9f5c3fe
pprzygodzk <48103797+pprzygodzk@users.noreply.github.com>
2019-07-23 21:43:34 +0200

    Update exp_rec.py

--- exp_rec.py
+++ exp_rec.py
@@ -1,13 +1,11 @@
 def exp_rec(a, n):
     """returns a to the n-th power (recursive way)"""
     try:
-        if n == 0:
-            return 1
-        elif n > 0:
+        exp = 1
+        if n > 0:
             exp = a * exp_rec(a, n-1)
-            return exp
-        else:
+        if n < 0:
             exp = (1/a) * exp_rec(a, n+1)
-            return exp
+        return exp
     except TypeError:
         print("Given arguments are the wrong type!")
