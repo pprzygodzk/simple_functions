f23c28890ad17eeec5552eb073c5791ba0f75c4f
pprzygodzk <48103797+pprzygodzk@users.noreply.github.com>
2019-08-12 16:35:36 +0200

    Update lcm.py

--- lcm.py
+++ lcm.py
@@ -3,17 +3,17 @@ from functools import reduce
 
 def LCM(a, b):
     """returns the least common multiple (LCM) of two integers a & b"""
-    try:
-        assert a > 0 and b > 0, "Both arguments need to be different than zero"
-        lcm = a*b/GCD_iter(a, b)
-        return int(lcm)
-    except TypeError:
-        print("Given arguments are the wrong type!")
+    
+    assert isinstance(a, int) and isinstance(b, int), "Given arguments have to be integer!"
+    assert a > 0 and b > 0, "Both arguments need to be different than zero"
+    lcm = a*b/GCD_iter(a, b)
+    return int(lcm)
         
 
 def LCM_n(*l):
     """returns the least common multiple of n integers: a1, a2, ..., an
     (recursive way)"""
+    
     try:
         if len(l) == 2:
             return LCM(l[0], l[1])
