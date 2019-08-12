f217c3a5107c333aff5b33fdcc8fdb7a9fcfe5bb
pprzygodzk <48103797+pprzygodzk@users.noreply.github.com>
2019-08-12 16:54:17 +0200

    Update newt_bin_coeff_iter.py

--- newt_bin_coeff_iter.py
+++ newt_bin_coeff_iter.py
@@ -1,11 +1,10 @@
-from silnia_iteracyjnie import silnia_iter
+from factorial_iter import fac_iter
 
 def Newt_bin_coeff_iter(n, k):
     """returns a value of a Newton binomial coefficient for given n & k
     (iterative way)"""
-    try:
-        assert k >= 0 and k <= n, "Arguments out of range!"
-        coefficient = silnia_iter(n) / (silnia_iter(k) * silnia_iter(n-k)) # silnia_iter is a function for factorial
-        return coefficient
-    except TypeError:
-        print("Given arguments are the wrong type!")
+    
+    assert isinstance(n, int) and isinstance(k, int), "Both arguments have to be integer!"
+    assert k >= 0 and k <= n, "Arguments out of range!"
+    coefficient = fac_iter(n) / (fac_iter(k) * fac_iter(n-k))
+    return coefficient
